00d1349d1c78db9bc4a9e74ab079e8563680e0fb
swezinoo <swezinoo410@gmail.com>
2025-03-24 12:47:45 -0600

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-
+import InstitutionCollaboration from "./pages/InstitutionCollaboration";
 function App() {
   return (
     <>
       <Router>
         <div>
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            {/* <Route path="/" element={<Dashboard />} /> */}
+            <Route path="/" element={<InstitutionCollaboration />} />
           </Routes>
         </div>
       </Router>
